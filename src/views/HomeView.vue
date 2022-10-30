5211368763e14eca812aa364ec653e2e4944dbc9
Dominic <dominic.kirste@gmail.com>
2022-10-30 22:54:28 +0100

    added job list

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,45 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+
+import { reactive, ref } from 'vue';
+import type Job from '@/types/Job'
+
+const jobs = ref<Job[]>([
+  {
+    title: 'farm worker',
+    location: 'lon lon ranch',
+    salary: 30000,
+    id: '1'
+  },
+  { 
+    title: 'quarryman', 
+    location: 'death mountain',
+    salary: 40000,
+    id: '2' 
+  },
+  { 
+    title: 'flute player', 
+    location: 'the lost woods', 
+    salary: 35000, 
+    id: '3' 
+  },
+  { 
+    title: 'fisherman', 
+    location: 'lake hylia', 
+    salary: 21000, 
+    id: '4' 
+  },
+  { 
+    title: 'prison guard', 
+    location: 'gerudo valley', 
+    salary: 32000, 
+    id: '5' 
+  }
+])
+
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+
   </main>
 </template>
